fc3abfef37df3097a1977ec5cf0b76c302814f8d
Salhotra <AKS396@pitt.edu>
2025-04-12 07:49:09 -0400

    added visualizer

--- visualizer/feature_maps_visualiser.py
+++ visualizer/feature_maps_visualiser.py
@@ -1,5 +1,5 @@
 #import model
-from model.resnet_3d import Resnet18_3D
+from model.sequence_model import Seq_Model
 #import dataloader
 from dataloader import OCTDataset,collate_fn
 
@@ -9,11 +9,12 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import torch
 import torch.nn as nn
+import sys
 
 def hook_func(name):      
     def hook_fn(module,input,output):
         global feature_maps
-        feature_maps[name]=output.detach().cpu().squeeze(dim=0)
+        feature_maps[name]=output.detach().cpu()[0]
     return hook_fn
 
 def register_hooks(model:str,model_ob:torch.nn.Module):
@@ -27,7 +28,7 @@ def register_hooks(model:str,model_ob:torch.nn.Module):
     elif model=='Seq_model_conv':
         layers=[f'seq_{i}'for i in range(4)]
         l_iter=iter(layers)
-        for layer in model_ob.feature_extractor():
+        for layer in model_ob.feature_extractor.children():
             if isinstance(layer,nn.Sequential):
                 if hasattr(layer[0],'conv2'):
                     layer[0].conv2.register_forward_hook(hook_func(next(l_iter)))
@@ -44,31 +45,33 @@ def on_key(event):
     key_pressed=event.key
     plt.close() 
     
-def get_plot(data):
-    ran=torch.randint(low=0,high=data.shape[0],size=(16,))
+def get_plot(features,data):
+    data=data[0].squeeze(dim=(0,1)).detach().cpu()
+    plt.imshow(data[0],cmap='gray')
+    ran=torch.randint(low=0,high=features.shape[0],size=(16,))
     
     fig, axs = plt.subplots(4, 4, figsize=(12, 6))
     fig.canvas.mpl_connect('key_press_event', on_key)
     
     for n, i in enumerate(ran):
-        axs[n//4, n%4].imshow(data[i], cmap='virdis')
+        axs[n//4, n%4].imshow(features[i], cmap='rainbow')
         axs[n//4, n%4].set_title(f'feature map number  #{i.item()}')
         axs[n//4, n%4].axis('off')
     
     plt.show()
         
 if __name__=="__main__":
-    json_file=""
-    data_type="" 
-    excel_path=""
-    transform=transforms.Compose([[transforms.ToTensor(),
-                                transforms.Resize((256,256))]])
+    json_file="jsons/train_test_val_split_without_scar_with_both_res.json"
+    data_type="train" 
+    excel_path="d:\\cleaning_GUI_annotated_data\\tab_data_annotated_pats.xlsx"
+    transform=transforms.Compose([transforms.ToTensor(),
+                                transforms.Resize((256,256))])
     num_classes=5
     batch_size=1
     timeout=600
     device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     save_features=False
-    parameters_path=""
+    parameters_path="model_parameter_Seq_Model\\8\\fold1_epoch10_val_6.4493_train_0.6559"
     
     assert any(data_type==n for n in ['test','train']),f"the value of data_type should be form [train,test] but it was found to be {data_type}"
     assert batch_size==1,f'batch size must be 1 for proper visualization'
@@ -76,9 +79,15 @@ if __name__=="__main__":
     with open(json_file,'r') as file:
         paths=json.load(file)
     
-    dataset=OCTDataset(paths[f'{data_type}_path'],excel_path,transform)
-    model=Resnet18_3D(num_classes).to(device)
-    model.load_state_dict(torch.load(parameters_path,device=device))
+    dataset=OCTDataset(paths[f'{data_type}_path'],excel_path,transform,attn=True)
+    model=Seq_Model(num_classes=5).to(device)
+    model_dict=torch.load(parameters_path,map_location=device)
+    model_dict = {
+    key.replace("module.", "", 1): value
+    for key, value in model_dict.items()
+    }
+
+    model.load_state_dict(model_dict)
     dataloader=DataLoader(dataset,batch_size,timeout,collate_fn=collate_fn)
     feature_maps={}
     register_hooks('Seq_model_conv',model)
@@ -91,20 +100,24 @@ if __name__=="__main__":
         
         pred=model(*data)
         
-        for key,data in feature_maps.items():
+        for key,features in feature_maps.items():
             print(key)
-            print(data.shape)
+            print(features.shape)
             
-            get_plot(data)
+            get_plot(features,data)
             
             if key_pressed == 'z':
                 print("Pressed 'z' — exiting loop.")
+                sys.exit(0)
+
+            if key_pressed=='n':
+                print('next volume')
                 break
 
             if key_pressed=='m':
-                get_plot(data)
+                get_plot(features,data)
                 while key_pressed=='m':
-                    get_plot(data)
+                    get_plot(features,data)
                     
             
             
