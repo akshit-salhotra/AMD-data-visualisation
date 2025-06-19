e5ea25d8c71c786b562893344bf39cf003ba6905
akshit-salhotra <salhotra.akshit@gmail.com>
2025-06-19 17:40:16 +0530

    2d rescons pipeline + bscan dataset class

--- pretraining_vision_model.py
+++ pretraining_vision_model.py
@@ -5,23 +5,21 @@ from utils.logger import intialise_logger_nd_create_folders
 from utils.util import load_model
 import torch
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader,Subset
+from torch.utils.data import DataLoader,Subset,random_split
 # from torch.nn import CrossEntropyLoss
 from memory_profiler import profile
-from dataloader import OCTDataset,collate_fn
+from dataloader import OCTDataset,collate_fn,B_ScanDataset
 import torchvision.utils as vutils
 import torch.optim as optim
 from model.resnet_3d import Resnet18_3D
 # from model.sequence_model import Seq_Model
 # from model.resnet_medicalnet import resnet10
-from model.auto_encoder import AutoEncoder
-# from utils.make_plots import get_batch_stats_plot
+from model.auto_encoder import AutoEncoder,AutoEncoder_2d
 from utils.util import SliceLevelPerceptualLoss
 import os
 from tqdm import tqdm
 import logging
 import json
-# from sklearn.model_selection import KFold
 import torch.nn as nn
 import math
 import re
@@ -29,9 +27,37 @@ from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-# from hooks.batch_hook import create_hook,batchnorm_stats
+import wandb
 
+def save_reconstructions_3d(args,recons_scans,scans,idx=None):
+    b_scans=[]
+    for index in range(min(4,args.batch)):
+        b_scans.append(recons_scans[index,0,args.save_bscans].unsqueeze(1))
+        b_scans.append(scans[index,0,args.save_bscans].unsqueeze(1))
+    # print('bscan shape ',len(b_scans),b_scans[0].shape)
+    b_scans=torch.concat(b_scans,dim=0)
+    if idx is None:
+        images = [wandb.Image(bscan, caption=f"Image {i}") for i, bscan in enumerate(b_scans)]
+        wandb.log({"reconstructed bscans": images})
+    else:
+        vutils.save_image(b_scans,recons_dir+os.sep+"_"+str(idx)+".png",normalize=True,nrow=args.save_bscans.shape[0])
 
+def save_reconstructions_2d(args,recons_scans,scans,idx=None):
+    b_scans=[]
+    # for index in range(min(8,args.batch)):
+    b_scans.append(recons_scans[:min(8,args.batch)])
+    b_scans.append(scans[:min(8,args.batch)])
+    b_scans=torch.concat(b_scans,dim=0)
+
+    if idx is None:
+        images = [wandb.Image(bscan, caption=f"Image {i}") for i, bscan in enumerate(b_scans)]
+        wandb.log({"reconstructions": images})
+
+    else:
+        vutils.save_image(b_scans,recons_dir+os.sep+"_"+str(idx)+".png",normalize=True,nrow=2)
+
+    
+    
 if __name__=="__main__":
     try:
     
@@ -55,12 +81,16 @@ if __name__=="__main__":
         parser.add_argument('--save_recons',type=bool,default=True,help="whether to save some reconstructions")
         parser.add_argument('--save_bscans',type=torch.Tensor,default=torch.tensor([36,48,60,72]),help="which reconstructed bscans to save")
         parser.add_argument('--class_dict',type=dict,default={'early':0,'inter':1,'ga':2,'wet':3,'notAMD':4})
+        parser.add_argument('--is_2D',type=bool,default=True)
 
         args = parser.parse_args()
         
         param_dir=intialise_logger_nd_create_folders(args)
+        run=wandb.init(project='Auto-encoder AMD'
+        ,config=vars(args))
         
-        model=AutoEncoder(args.model_ch).to(args.device)
+        # model=AutoEncoder(args.model_ch).to(args.device)
+        model=AutoEncoder_2d().to(args.device)
         model=nn.DataParallel(model)
         logging.info(f'found {torch.cuda.device_count()} gpus!')
         logging.info(model)
@@ -73,30 +103,57 @@ if __name__=="__main__":
         if args.model_path:
             load_model(args,model)
             
-        with open(args.json,'r') as file:
-            paths= json.load(file) 
-        train_paths=paths['train_path']
-        val_paths=paths['test_path']
+        if not args.is_2D:
+            
+            with open(args.json,'r') as file:
+                paths= json.load(file) 
+            train_paths=paths['train_path']
+            val_paths=paths['test_path']
+        
         transform=transforms.Compose([transforms.ToTensor(),
                                     #   transforms.RandomHorizontalFlip(),
-                                      transforms.Resize((256,256))])
+                                    transforms.Resize((256,256),
+                                    transforms.RandomAffine(degrees=5, translate=(0.05, 0.05)),
+                                    transforms.Lambda(lambda x: x + 0.01 * torch.randn_like(x)))])
         
-        train_dataset=OCTDataset(val_paths,args.excel_path,transform,attn=False,undersample=True,classes=args.class_dict)
-        val_dataset=OCTDataset(val_paths,args.excel_path,transform,attn=False,undersample=False,classes=args.class_dict)
+        if not args.is_2D:
+            dataset_config={'transforms':transform,
+                            'attn':False,
+                            'undersample':True,
+                            'denoise':False,
+                            'classes':args.class_dict}
+            
+            train_dataset=OCTDataset(val_paths,args.excel_path,**dataset_config)
+            val_dataset=OCTDataset(val_paths,args.excel_path,**dataset_config)
         # train_dataset = Subset(train_dataset, indices=range(4))
+        else:
+            denoise=False
+            collate_fn=None
+            dataset_config={'transform':transform,
+                            'denoise':denoise}
+            dataset=B_ScanDataset(transform,args.excel_path,denoise)
+            val_percent = 0.10
+            val_size = int(len(dataset) * val_percent)
+            train_size = len(dataset) - val_size
 
-        train_loader = DataLoader(train_dataset, batch_size=args.batch, shuffle=True,num_workers=0,timeout=0,collate_fn=collate_fn)
-        test_loader = DataLoader(train_dataset, batch_size=args.batch, shuffle=False,num_workers=0,timeout=0,collate_fn=collate_fn)
+            generator = torch.Generator().manual_seed(42)
+
+            train_dataset, val_dataset = random_split(dataset, [train_size, val_size], generator=generator)
+            print('number of scans in train set :',len(train_dataset))
+            print('number of scans in val set',len(val_dataset))
+
+        run.config.update(dataset_config)
+        train_loader = DataLoader(train_dataset, batch_size=args.batch, shuffle=True,num_workers=15,timeout=300,collate_fn=collate_fn)
+        test_loader = DataLoader(train_dataset, batch_size=args.batch, shuffle=False,num_workers=15,timeout=300,collate_fn=collate_fn)
         
         
         
         if args.model_path and re.search(r'fold(\d+)',args.model_path):
-            # if fold<int(re.search(r'fold(\d+)',args.model_path).group(1)):
-                # continue
-            # elif fold==int(re.search(r'fold(\d+)',args.model_path).group(1)):
-                match=re.search(r'epoch(\d+)',args.model_path)
-                start_epoch=int(match.group(1))+1
-                pbar=tqdm(range(start_epoch,args.epoch))
+
+            match=re.search(r'epoch(\d+)',args.model_path)
+            start_epoch=int(match.group(1))+1
+            pbar=tqdm(range(start_epoch,args.epoch))
+            
         else:
             pbar=tqdm(range(args.epoch))
 
@@ -106,25 +163,35 @@ if __name__=="__main__":
                 percep_loss=0
                 model.train()
 
-                def train(data,recons_loss,percep_loss,epoch_loss):
-                    
-                        # print('hi...')
-                        scans=data[0].to(args.device)
-                        # print(torch.cuda.memory_summary())
+                def train(args,data,recons_loss,percep_loss,epoch_loss):
+
+                        if not args.is_2D:
+                            scans=data[0].to(args.device)
+                        else:
+                            scans=data.to(args.device)
+                            
                         recons_scans=model(scans)
-                        # print('bye')
-                        # print(torch.cuda.memory_summary())
 
                         r_loss=recons_criteron(recons_scans,scans)
-                        p_loss=0
-                        # p_loss=percep_criteron(recons_scans.reshape(-1,1,256,256),scans.reshape(-1,1,256,256))
+                        if not args.is_2D:
+                            assert recons_scans.shape[-2]==(256,256),f' shape is not right , found the shape to be :{recons_scans.shape}'
+                            p_loss=percep_criteron(recons_scans.reshape(-1,1,256,256),scans.reshape(-1,1,256,256))
+                        else:
+                            p_loss=percep_criteron(recons_scans,scans)
+
                         # print('calculating loss')
                         loss=r_loss+args.lambda_percep*p_loss
                         # print(torch.cuda.memory_summary())
                         # print('....')
                         if iter% args.log_freq==0:
                             logging.info(f'Epoch:{i}/{args.epoch} iteration:{iter}/{math.ceil(len(train_dataset)/args.batch)} Loss is :{loss:.4f} reconstruction loss :{r_loss:.4f} perceptual loss :{p_loss:.4f}')
-
+                        
+                        if iter%(args.log_freq*5)==0:
+                            if args.is_2D:
+                                save_reconstructions_2d(args,recons_scans,scans)
+                            else:
+                                save_reconstructions_3d(args,recons_scans,scans)
+                                
                         epoch_loss+=loss
                         recons_loss+=r_loss
                         percep_loss+=p_loss
@@ -133,33 +200,22 @@ if __name__=="__main__":
                         optimizer.step()
                         optimizer.zero_grad()
                         return epoch_loss,percep_loss,recons_loss
-#                 with torch.profiler.profile(schedule=torch.profiler.schedule(
-#         wait=1,        # don't record the first step
-#         warmup=1,      # record shapes and memory after warmup
-#         active=2       # record 2 steps of real data
-#     ),
-#     activities=[
-#         torch.profiler.ProfilerActivity.CPU,
-#         torch.profiler.ProfilerActivity.CUDA],
-#     profile_memory=True,
-#     record_shapes=True,
-#     with_stack=True
-# ) as prof:
+
                 for iter,(data,_,_) in tqdm(enumerate(train_loader)):
 
-                        epoch_loss,percep_loss,recons_loss=train(data,recons_loss,percep_loss,epoch_loss)
-                        # prof.step()
-                        
-                    # print(prof.key_averages().table(sort_by="self_cuda_memory_usage", row_limit=20))
+                        epoch_loss,percep_loss,recons_loss=train(args,data,recons_loss,percep_loss,epoch_loss)
+
 
                 epoch_loss/=len(train_dataset)
                 percep_loss/=len(train_dataset)
                 recons_loss/=len(train_dataset)
-
-                logging.info(f'Epoch:{i}/{args.epoch} Loss is :{epoch_loss:.4f} reconstruction loss :{recons_loss:.4f} perceptual loss :{percep_loss:.4f}')
-                pbar.set_postfix({'average epoch loss':f'{epoch_loss:.4f}'
+                train_metrics={'average epoch loss':f'{epoch_loss:.4f}'
                                   ,'average r loss':f'{recons_loss:.4f}'
-                                  ,'average p loss':f'{percep_loss:.4f}'})
+                                  ,'average p loss':f'{percep_loss:.4f}'}
+                
+                logging.info(f'Epoch:{i}/{args.epoch} Loss is :{epoch_loss:.4f} reconstruction loss :{recons_loss:.4f} perceptual loss :{percep_loss:.4f}')
+                pbar.set_postfix(train_metrics)
+                wandb.log(train_metrics)
                 scheduler.step()
                 
                 if i%args.save_freq==0 or args.epoch==i+1:
@@ -170,12 +226,20 @@ if __name__=="__main__":
                     
                     with torch.no_grad():
                         for i,(data,_,_) in enumerate(test_loader):
-                            
-                            scans=data[0].to(args.device)
+                            if not args.is_2D:
+                                scans=data[0].to(args.device)
+                            else:
+                                scans=data.to(args.device)
+                                
                             recons_scans=model(scans)
                             val_r_loss=recons_criteron(recons_scans,scans)
-                            # val_p_loss=percep_criteron(recons_scans,scans)
-                            val_p_loss=0
+                            
+                            if args.is_2D:
+                                val_p_loss=percep_criteron(recons_scans,scans)
+                            else:
+                                val_p_loss=percep_criteron(recons_scans.reshape(-1,1,256,256),scans.reshape(-1,1,256,256))
+
+                                
                             loss=val_r_loss +args.lambda_percep*val_p_loss
                             
                             val_loss+=val_loss
@@ -185,25 +249,34 @@ if __name__=="__main__":
                         if args.save_recons:
                             recons_dir=param_dir+os.sep+"reconstructions"+os.sep+f"epoch_{i}"
                             os.makedirs(recons_dir,exist_ok=True)
-                            b_scans=[]
+                            # b_scans=[]
                             
-                            for index in range(min(4,args.batch)):
-                                b_scans.append(recons_scans[index,0,args.save_bscans].unsqueeze(1))
-                                b_scans.append(scans[index,0,args.save_bscans].unsqueeze(1))
+                            if not args.is_2D:
+                                save_reconstructions_3d(args,recons_scans,scans,i)
+                            else:
+                                save_reconstructions_2d(args,recons_scans,scans,i)
+                                # for index in range(min(4,args.batch)):
+                                #     b_scans.append(recons_scans[index,0,args.save_bscans].unsqueeze(1))
+                                #     b_scans.append(scans[index,0,args.save_bscans].unsqueeze(1))
 
-                            vutils.save_image(b_scans,recons_dir+os.sep+str(i)+".png",normalize=True,nrow=args.save_bscans.shape[0])
+                                # vutils.save_image(b_scans,recons_dir+os.sep+str(i)+".png",normalize=True,nrow=args.save_bscans.shape[0])
                             
                         val_loss/=math.ceil(len(val_dataset)/args.batch)
                         val_recons_loss/=math.ceil(len(val_dataset)/args.batch)
                         val_percep_loss/=math.ceil(len(val_dataset)/args.batch)
 
                         logging.info(f'VAL loss :{val_loss:.4f} reconstruction loss :{val_recons_loss:.4f} perceptual loss :{val_percep_loss:.4f}')
-
+                        wandb.log({
+                            'val loss':val_loss,
+                            'val recons loss':val_recons_loss,
+                            'val perceptual loss':val_percep_loss
+                        })
                         torch.save(model.state_dict(),f'{param_dir}/epoch{i}_val_{val_loss:.4f}_train_{epoch_loss:.4f}')
 
             
         print('training completed !!')
         logging.info('training complete!!!')
+        wandb.finish()
 
     except Exception as e:
         print(e)
